743edb147399a4040cc1c81a732a99a698addef5
Manish <183129912+Manish-c09p@users.noreply.github.com>
2026-08-13 12:51:42 +0000

    Implement calculating functions in console

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Fuck!");
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const division = (a, b) => a / b; 
+
+let first = Number(prompt("Please Enter a: "));
+let next = Number(prompt("Please Enter b: "));
+
+function operate () {
+    
+}
+
+let addision = add(first, next);
+let substraction = sub(first, next);
+let multiplication = multiply(first, next);
+let divide = division(first, next);
+
+console.log(addision);
+console.log(substraction);
+console.log(multiplication);
+console.log(divide);
